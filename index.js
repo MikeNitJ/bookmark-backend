bf93ce58286399ce05090031a733e3dc9c0d6dfb
Renz Lacerna <renzlacerna13@gmail.com>
2023-09-20 19:10:55 -0400

    backend connected

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
 const PORT = 3000
+const morgan = require("morgan");
+// const bookmarkController = require("./controller/bookmarkController")
+const bookmarkController = require("./controller/bookmarkController");
+app.use(morgan("tiny"));
+app.use(express.urlencoded({ extended: true}));
 
+app.use("/book", bookmarkController);
 
 app.listen(PORT, () => console.log(PORT, "is coming"))
