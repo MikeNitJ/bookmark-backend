55ed41428b970380ca7743b828ad612df31b1774
mikenitj <mikenitipong@hotmail.com>
2023-09-24 00:20:03 +0700

    add cors and s

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = 4000
 const morgan = require("morgan");
+const cors = require("cors")
 // const bookmarkController = require("./controller/bookmarkController")
 const bookmarkController = require("./controller/bookmarkController");
 app.use(morgan("tiny"));
+app.use(cors())
 app.use(express.urlencoded({ extended: true}));
 
 app.use("/book", bookmarkController);
